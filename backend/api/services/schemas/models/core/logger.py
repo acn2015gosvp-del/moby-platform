8c2971b4fe99749c12a6d589d7475a1052cd9596
siwan <acn2015gosvp@gmail.com>
2025-11-14 21:07:07 +0900

    Delete backend/api/services/schemas/models/core directory

--- backend/api/services/schemas/models/core/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger("moby")
-logger.setLevel(logging.INFO)
-
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
-
-logger.addHandler(handler)
